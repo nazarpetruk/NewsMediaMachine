a7a103984f87c90cc6edf3b472f6d5ddf33df33c
nazar petruk <nazarpetruk15@gmail.com>
2019-10-03 20:18:10 +0200

    -adding values to cell elements - adding KingFisher for img download and cache

--- NewsMediaMachine/Networking.swift
+++ NewsMediaMachine/Networking.swift
@@ -8,11 +8,12 @@
 
 import Foundation
 import Alamofire
+import DocumentClassifier
 
 
 class NewsCreator {
     
-    func getArticles (){
+    func getArticles (returnArticles : @escaping ([Article]) -> Void){
         Alamofire.request("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=deec1dac677344dfa61ae61c4c103975").responseJSON { (response) in
             if let json = response.result.value as? [String : Any] {
                 if let jsonArticles = json["articles"] as? [[String : Any]] {
@@ -22,13 +23,19 @@ class NewsCreator {
                             let urlToImg = jsonArticle["urlToImage"] as? String,
                             let articleUrl = jsonArticle["url"] as? String,
                             let description = jsonArticle["description"] as? String else { continue }
+                        
                         let article = Article()
                         article.title = title
                         article.urlToImg = urlToImg
                         article.articleUrl = articleUrl
                         article.description = description
+                        
+                        guard let classification = DocumentClassifier().classify(title + description) else { return }
+                        article.description = classification.prediction.category.rawValue
+                        
                         articles.append(article)
                     }
+                    returnArticles(articles)
                 }
             }
         }
