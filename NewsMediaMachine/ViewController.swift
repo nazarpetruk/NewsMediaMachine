bb96584f0d979132a530475f0aaf1f7dfd909df2
nazar petruk <nazarpetruk15@gmail.com>
2019-10-03 16:13:28 +0200

    Initial Commit

---
+++ NewsMediaMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsMediaMachine
+//
+//  Created by Nazar Petruk on 03/10/2019.
+//  Copyright © 2019 Nazar Petruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
